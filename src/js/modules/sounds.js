dc50cc35606d0ed1e7f251f4bb3df025f796316c
David Paul <davidpauljunior@gmail.com>
2017-06-23 16:29:40 +0100

    Switches to async await, and makes cleaner use of promise (only creates new promise the one time it's required), adds rhythmic boombasticness

--- src/js/modules/sounds.js
+++ src/js/modules/sounds.js
@@ -1,36 +1,6 @@
 // Sets up audio context
 const audioContext = new window.AudioContext();
 
-function playRhythm(soundsArray) {
-
-    console.log(Array.isArray(soundsArray));
-    console.log(soundsArray[0]);
-    console.log(soundsArray[1]);
-    console.log(soundsArray[2]);
-    const hiHat = soundsArray[0];
-    const kick = soundsArray[1];
-    const snare = soundsArray[2];
-
-    // TODO: Loop through soundsArray.
-    // Start playing the rhythm 100ms from "now" (now being when you click);
-    const startTime = audioContext.currentTime + 0.100;
-    const tempo = 120; // Beats per minute
-    const quarterNoteTime = 60 / tempo;
-
-    const playButton = document.querySelector('[data-hook="play-button"]');    
-    playButton.addEventListener('click', () => {
-        playSound(kick, startTime);
-        // playSound(hiHat, startTime);
-        playSound(kick, startTime + quarterNoteTime);
-        playSound(hiHat, startTime + 2*quarterNoteTime);
-        playSound(kick, startTime + 3*quarterNoteTime);
-        // playSound(snare, startTime);
-        // soundsArray.forEach(sound => {
-        //     playSound(sound, startTime + Math.random());
-        // })
-    });
-}
-
 function playSound(buffer, time) {
     /**
      * https://joshondesign.com/p/books/canvasdeepdive/chapter12.html
@@ -48,78 +18,75 @@ function playSound(buffer, time) {
     sourceNode.start(time);
 }
 
-// TODO: Store the sounds in localStorage
-function fetchSound(file) {
+function playRhythm(soundsArray) {
+    const hiHat = soundsArray[0];
+    const kick = soundsArray[1];
+    const snare = soundsArray[2];
 
-    // fetch(`../dist/audio/hi-hat.wav`)
-    //     .then(response => {
-    //         return response.arrayBuffer();
-    //     })
-    //     .then(buffer => {
-    //         audioContext.decodeAudioData(buffer, decodedData => {
-    //             playRhythm(decodedData);
-    //         });
-    //     });
+    const tempo = 200; // Beats per minute
+    const quarterNoteTime = 60 / tempo;
+    const eighthNoteTime = quarterNoteTime / 2;
 
-    /**
-     * https://developers.google.com/web/fundamentals/getting-started/primers/promises
-     * The promise constructor takes one argument, a callback with two parameters, resolve and reject. 
-     * Do something within the callback, perhaps async, then call resolve if everything worked, otherwise call reject.
-     */
-    return new Promise((resolve, reject) => {
+    const playButton = document.querySelector('[data-hook="play-button"]');
 
-        // Should this be an if (successful response.
-        fetch(`../dist/audio/${file}.wav`)
-            .then(response => {
-                    if (response.status !== 200) {  
-                        console.log(`Looks like there was a problem. Status Code: ${response.status}`);  
-                        return;  
-                    }
+    playButton.addEventListener('click', () => {
+        const startTime = audioContext.currentTime;
 
-                    response.arrayBuffer()
-                        .then(buffer => {
-                            audioContext.decodeAudioData(buffer, decodedData => {
-                                resolve(decodedData); // TODO: check this (can it be a return?)
-                            });
-                        })
-            })
-        
-            // Where do I put the reject?
-            .catch(err => {
-                console.log(`fetch error: ${err}`)
-            });
+        playSound(hiHat, startTime);
+        playSound(kick, startTime);
+        playSound(kick, startTime + eighthNoteTime);
+        playSound(hiHat, startTime + quarterNoteTime);
+        playSound(hiHat, startTime + 2*quarterNoteTime);
+        playSound(snare, startTime + 2*quarterNoteTime);
+        playSound(hiHat, startTime + 3*quarterNoteTime);
+        playSound(kick, startTime + 3*quarterNoteTime + eighthNoteTime);
+        playSound(hiHat, startTime + 4*quarterNoteTime);
+        playSound(kick, startTime + 4*quarterNoteTime + eighthNoteTime);
+        playSound(hiHat, startTime + 5*quarterNoteTime);
+        playSound(kick, startTime + 5*quarterNoteTime + eighthNoteTime);
+        playSound(hiHat, startTime + 6*quarterNoteTime);
+        playSound(snare, startTime + 6*quarterNoteTime);
     });
-        
-    // const files = soundFiles.map(sound => {
-    //     return new Promise((resolve, reject) => { 
-    //         fetch(`../dist/audio/${sound}.wav`).then(response => {
-    //             return response.arrayBuffer();
-    //         })
-    //         .then(buffer => {
-    //             audioContext.decodeAudioData(buffer, decodedData => {
-    //                 resolve(decodedData);
-    //             });
-    //         });
-    //      });
-    // });
+}
+
+function decodeAudio(buffer) {
+    return new Promise((resolve) => {
+        audioContext.decodeAudioData(buffer, decodedData => {
+            resolve(decodedData);
+        });
+    });
+
+    // TODO: Research if need reject (because it's a native method)
+        // reject(new Error(`Resonse went boom, status: ${response.status}`));
+}
+
+// TODO: Store the sounds in localStorage
+async function fetchSound(file) {
+    const response = await fetch(`../dist/audio/${file}.wav`);
+
+    if (response.status !== 200) {
+        // Throw error because that's how .catch in init would get it
+        throw new Error(`Resonse went boom, status: ${response.status}`);
+    }
+
+    const buffer = await response.arrayBuffer();
     
-    // Promise.all(files).then(sounds => {
-    //     playRhythm(sounds);
-    // });
+    return decodeAudio(buffer);
 }
 
 function init() {
     const soundFiles = ['hi-hat', 'kick', 'snare'];
 
-    // This returns the audio buffers but not in an array.
-    soundFiles.map(file => {
-        fetchSound(file).then(sound => {
-            console.log(sound);
-        })
+    const soundPromises = soundFiles.map(file => {
+        return fetchSound(file);
     });
 
-    //this line works -> gives the audio buffer
-    fetchSound('hi-hat').then(sound => {console.log(sound)});
+    Promise.all(soundPromises).then(soundPromise => {
+        console.log(soundPromise);
+        playRhythm(soundPromise);
+    }).catch(err => {
+        console.log(`fetch error: ${err}`);
+    });
 }
 
 module.exports = {init: init};
