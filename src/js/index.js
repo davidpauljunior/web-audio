706c76e04a14363596b6a5c603d58fc74ace8d44
David Paul <davidpauljunior@gmail.com>
2017-10-23 23:09:43 +0100

    Removes comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
-// const playRhythm = require('./modules/playRhythm');
-// const getSounds = require('./modules/getSounds');
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     async function init() {
-//         const sounds = await getSounds.init();
-//         // body.classList.remove('loading');
-
-//         console.log(sounds);
-//         playRhythm.init(sounds);
-//     }
-
-//     init();
-// });
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
